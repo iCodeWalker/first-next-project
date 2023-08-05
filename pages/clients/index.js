e2b3ae07be7853618658f88ba2038823d9f92ed7
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-08-05 17:21:53 +0530

    added : navigation with link component

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,9 +1,37 @@
 import React from "react";
+import Link from "next/link";
 
 const ClientsPage = () => {
+  const clients = [
+    { id: "11", name: "Satyam" },
+    { id: "32", name: "Vishesh" },
+    { id: "13", name: "Yash" },
+    { id: "45", name: "Abhinav" },
+  ];
   return (
     <div>
       <h1>Client Page</h1>
+      <ul>
+        {clients.map((item, index) => {
+          return (
+            // <li key={index}>
+            //   <Link href={`/clients/${item.id}`}>{item.name}</Link>
+            // </li>
+            // Different way to use Link / Alternative way
+
+            <li key={index}>
+              <Link
+                href={{
+                  pathname: "clients/[clientId]",
+                  query: { clientId: item.id },
+                }}
+              >
+                {item.name}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import React from "react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <h1>Index.js file</h1>
+      <h1>Home Page</h1>
+      <ul>
+        {/* <li>
+          <a href="/portfolio">Portfolio</a>
+        </li>
+        // not using this anchor tag as it sends a http request and all over states gets lost */}
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </>
   );
 }
