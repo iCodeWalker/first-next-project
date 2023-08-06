fb7ececeb9bbec0fe79c108d703b396e5567f65c
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-08-06 18:51:15 +0530

    added : navigation programmatically and custom 404 page

--- pages/clients/[clientId]/index.js
+++ pages/clients/[clientId]/index.js
@@ -6,9 +6,23 @@ const ClientProjectsPage = () => {
   const router = useRouter();
 
   console.log(router.query);
+
+  function clickHandler() {
+    // router.push("/clients/13/new-project");
+    // alternative way
+
+    router.push({
+      pathname: "/clients/[clientId]/[clientProjectId]",
+      query: {
+        clientId: "13",
+        clientProjectId: "Project-A",
+      },
+    });
+  }
   return (
     <div>
       <h1>Client Projects Page</h1>
+      <button onClick={clickHandler}>Load Project A</button>
     </div>
   );
 };
